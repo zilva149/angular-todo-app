a5abc2896fdbd67c3d5a5b7638bc10c0d01cd764
zilva149 <114057257+zilva149@users.noreply.github.com>
2023-07-22 15:26:17 +0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
+import { CockpitComponent } from './components/cockpit/cockpit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoItemComponent,
+    CockpitComponent
   ],
   imports: [
     BrowserModule
